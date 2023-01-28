0f1ca6ff3f308a8950f9d14c56c6233d2165f5b5
mdansarulislam641 <mdansarulislam641.com>
2023-01-28 15:25:54 +0600

    redux assign and install

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { router } from './Routes/Router';
 
 function App() {
   return (
-    <div className="max-screen-w-xl mx-auto">
+    <div className="max-w-screen-xl mx-auto">
       <RouterProvider router={router}>
 
       </RouterProvider>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ProductCard from './ProductsCard';
 
 const Home = () => {
+    const [products , setProducts] = useState([])
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res=>res.json())
+        .then(data =>setProducts(data))
+    },[])
     return (
-        <div>
-            <h2>this is home</h2>
+        <div className='grid grid-cols-3 gap-5 space-y-3'>
+           {
+            products?.map((product,idx) => <ProductCard
+                key={idx}
+                product ={product}
+            ></ProductCard>)
+           }
         </div>
     );
 };
