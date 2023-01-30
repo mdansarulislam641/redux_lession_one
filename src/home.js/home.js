e5fe965003787669b3fa0a046fcb6b2aa6dc788f
Farjana Islam <farjanaanika2@gmail.com>
2023-01-30 01:11:30 -0800

    s

--- src/home.js/home.js
+++ src/home.js/home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const home = () => {
     return (
         <div>
-            <h1>hello there</h1>
+            <h1>hello there i am anika</h1>
         </div>
     );
 };
